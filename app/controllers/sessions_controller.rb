331cbdab8ab6798bccb2541aab685b5b74c1121f
Ko Tashiro <tai1fuu7@gmail.com>
2020-05-03 16:51:34 +0900

    TOPページ追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'ログインしました。'
+      redirect_to orders_path, notice: 'ログインしました。'
     else
       render :new
     end
